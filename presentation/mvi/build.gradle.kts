f6a2907d3cb17c89447912a30bf40bfeeb9375c9
seg <segnities007@gmail.com>
2025-10-05 14:14:49 +0900

    create mvi

--- presentation/mvi/build.gradle.kts
+++ presentation/mvi/build.gradle.kts
@@ -37,6 +37,15 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    
+    // ViewModel & Lifecycle
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    
+    // Kotlin Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
