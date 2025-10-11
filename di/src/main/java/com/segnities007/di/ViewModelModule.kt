d4fc470a7de9ad0aa7b1ae82ea2b746a0e6a7fcc
seg <segnities007@gmail.com>
2025-10-11 20:23:52 +0900

    refactoring

--- di/src/main/java/com/segnities007/di/ViewModelModule.kt
+++ di/src/main/java/com/segnities007/di/ViewModelModule.kt
@@ -5,14 +5,5 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    // Auth ViewModels
-    viewModel { SignUpViewModel(signUpUseCase = get()) }
-    
-    // TODO: 他のViewModelを追加
-    // viewModel { SignInViewModel(signInWithGoogleUseCase = get()) }
-    // viewModel { TimelineViewModel(getTimelineUseCase = get()) }
-    // viewModel { PostViewModel(createPostUseCase = get()) }
-    // viewModel { ProfileViewModel(getMyProfileUseCase = get(), updateProfileUseCase = get()) }
-    // viewModel { SearchViewModel(searchUsersUseCase = get()) }
-    // viewModel { SettingsViewModel(signOutUseCase = get()) }
+    // TODO: ViewModelを追加
 }
